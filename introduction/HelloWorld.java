ee025e3d2b015e14b9e0e9c29fe67bdc59ec91f5
harshita jain847 <hj.harshitabadkul@gmail.com>
2025-11-04 07:18:15 +0530

    java

---
+++ introduction/HelloWorld.java
@@ -0,0 +1,14 @@
+// compiler for java-JDK [java development kit]
+// extension-.java
+// java is platform independent language
+// jdk has two components- 1. JVM 2. JRE
+
+// boiler plate code
+
+
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.println("hello java ");
+    }
+}
