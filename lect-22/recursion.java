2a5fdcd1edee5383a30806d8351aaeee0450ae36
harshita jain847 <hj.harshitabadkul@gmail.com>
2026-05-23 22:18:29 +0530

    java

---
+++ lect-22/recursion.java
@@ -0,0 +1,10 @@
+public class recursion {
+    public static int b(int n,string current){
+        if(current.length()==n){
+        return n;
+        }
+
+        return (,"0");
+        return (n,"1");
+    }
+}
